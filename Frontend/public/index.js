46a15997db34abca95dbe47af01ca4288e36571e
Håkan Öström <hakan.ostrom@gmail.com>
2024-04-26 19:17:23 +0200

    bokningsdetaljer

--- Frontend/public/index.js
+++ Frontend/public/index.js
@@ -6,33 +6,41 @@ const listaStugor = async() => {
     let response = await fetch('http://localhost:8081/stuga')
     let resJson = await response.json()
     resJson.forEach(element => {
-        let option = new Option(element.name, element.id);
-        stugorSelect.add(option);
+        let option = new Option(element.name, element.id)
+        stugorSelect.add(option)
     });   
 }
 
 const bokaStuga = async() => {
     let stugorSelect = document.getElementById("stugorSelect")
     
-    let bokning = {
-        ID: 34,
-        stuga_id: stugorSelect.selectedIndex,
-        namn: "Abralon",
-        telefon:"swerw",
-        epost: "sdfg"
+    let bokareNamn = document.getElementById("bokareNamn").value
+    let bokareTelefon = document.getElementById("bokareTelefon").value
+    let bokareEpost = document.getElementById("bokareEpost").value
+    let bokningsdatum = document.getElementById("bokningsdatum").value
+
+    if(bokareNamn== "" || bokareTelefon == "" || bokareEpost == "")
+      alert("Fyll i samtliga bokningsuppgifter")
+    else{
+        let bokning = {
+            stuga_id: stugorSelect.selectedIndex,
+            namn: bokareNamn,
+            telefon: bokareTelefon,
+            epost: bokareEpost,
+            datum: bokningsdatum
+        }
+
+        let response = await fetch('http://localhost:8081/bokning', {
+        method: 'POST',
+        body: JSON.stringify(bokning),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      let resJson = await response.json()
+      document.getElementById("coonfirmationPlaceholder").innerText = "Tack för din bokning av stuga nummer " + resJson.stuga_id
     }
-
-    let response = await fetch('http://localhost:8081/bokning', {
-    method: 'POST',
-    body: JSON.stringify(bokning),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-
-
-    let resJson = await response.json()
-    document.getElementById("coonfirmationPlaceholder").innerText = "Tack för din bokning av stuga nummer " + resJson.stuga_id
 }
 
 
