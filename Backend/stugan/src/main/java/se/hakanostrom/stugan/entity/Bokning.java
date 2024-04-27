53cff11ad91f733a9bf9e06d8bf3287880e26d1c
Håkan Öström <hakan.ostrom@gmail.com>
2024-04-27 16:53:06 +0200

    builder pattern

--- Backend/stugan/src/main/java/se/hakanostrom/stugan/entity/Bokning.java
+++ Backend/stugan/src/main/java/se/hakanostrom/stugan/entity/Bokning.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Table(name = "bokningar")
 @Entity
-@Data
 public class Bokning {
     @Id
     @GeneratedValue
@@ -19,4 +17,58 @@ public class Bokning {
     private String namn;
     private String telefon;
     private String epost;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public Bokning setID(Long ID) {
+        this.ID = ID;
+        return this;
+    }
+
+    public Long getStuga_id() {
+        return stuga_id;
+    }
+
+    public Bokning setStuga_id(Long stuga_id) {
+        this.stuga_id = stuga_id;
+        return this;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public Bokning setDatum(String datum) {
+        this.datum = datum;
+        return this;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public Bokning setNamn(String namn) {
+        this.namn = namn;
+        return this;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public Bokning setTelefon(String telefon) {
+        this.telefon = telefon;
+        return this;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public Bokning setEpost(String epost) {
+        this.epost = epost;
+        return this;
+    }
 }
--- Backend/stugan/src/main/java/se/hakanostrom/stugan/entity/Stuga.java
+++ Backend/stugan/src/main/java/se/hakanostrom/stugan/entity/Stuga.java
@@ -4,16 +4,32 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Table(name = "stugor")
 @Entity
-@Data
 public class Stuga {
 
     @Id
     @GeneratedValue
     private Long ID;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Stuga setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public Stuga setID(Long ID) {
+        this.ID = ID;
+        return this;
+    }
 
 }
